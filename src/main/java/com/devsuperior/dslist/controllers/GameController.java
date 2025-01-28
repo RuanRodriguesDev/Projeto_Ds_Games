33a24409ec66133c8850d8df3a224550f86b8d40
Ruan Rodrigues <rupriwell123@outlook.com>
2025-01-28 12:07:06 -0300

    Create GamaDto

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDto;
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.services.GameService;
+
 @RestController
 @RequestMapping(value = "/games")
 public class GameController {
@@ -18,6 +21,12 @@ public class GameController {
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id) {
+        GameDto result = gameService.findById(id);
+        return result;
+    }
+    
 
     @GetMapping
     public List<GameMinDto>findAll(){
