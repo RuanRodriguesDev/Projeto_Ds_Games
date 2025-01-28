1ad66aa5b3dc9bc1b42a881b6d227d7f68df0e95
Ruan Rodrigues <rupriwell123@outlook.com>
2025-01-28 09:47:34 -0300

    Create GamaList,Belonging,BelongingPk

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.services.GameService;
-
+@RestController
+@RequestMapping(value = "/games")
 public class GameController {
     @Autowired
     private GameService gameService;
